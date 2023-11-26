940a57f9040981241bc4a473e09faaf9ebc040a8
Sunnamsriram <59051820+sunnamsriram1@users.noreply.github.com>
2023-11-26 18:12:41 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,28 @@
+# Update Script for UserRecon v2.1-Stable
+# Script created by @Sunnam01ram
+
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... Unable to update ..."; exit 1; }
+
+}
+
+script() {
+
+clear
+printf "\n \e[1;92mUpdating \e[1;94mUserRecon\e[1;92m ...\n\n"
+sleep 1.5
+cd ..
+rm -rf FindUser_URLs
+git clone https://github.com/sunnamsriram1/FindUser_URLs.git
+cd FindUser_URLs
+chmod +x userurl.sh
+printf "\n\e[1;92mRestarting ...\n\e[0m"
+bash userurl.sh
+cd ..
+
+}
+
+dependencies
+script
